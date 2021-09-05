82d101085062ea40684c9eee9f46d30956451c9d
Alex Eidt <alex.eidt@outlook.com>
2021-09-04 21:41:15 -0700

    Removed SeamManager

--- src/SeamManager.java
+++
@@ -1,8 +0,0 @@
-public class SeamManager {
-    private SeamCarver horizontal, vertical;
-
-    public SeamManager(SeamCarver horizontal, SeamCarver vertical) {
-        this.horizontal = horizontal;
-        this.vertical = vertical;
-    }
-}
