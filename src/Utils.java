6adb2d2904dc835f2fcd19fe94aad13b90c12171
Alex Eidt <alex.eidt@outlook.com>
2021-11-26 19:48:33 -0800

    Fixed grayscaling in Utils

--- src/Utils.java
+++ src/Utils.java
@@ -53,8 +53,8 @@ public class Utils {
         for (int h = 0; h < height; h++) {
             for (int w = 0; w < width; w++) {
                 int pixel = image[h][w];
-                int r = (pixel << 16) & 0xFF;
-                int g = (pixel << 8) & 0xFF;
+                int r = (pixel >> 16) & 0xFF;
+                int g = (pixel >> 8) & 0xFF;
                 int b = pixel & 0xFF;
                 gray[h][w] = (int) (0.299 * r + 0.587 * g + 0.114 * b);
             }
