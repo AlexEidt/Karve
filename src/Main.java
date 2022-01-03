7e00ae8b0788d6b919fa14f40cda386c41704323
Alex Eidt <alex.eidt@outlook.com>
2022-01-03 15:58:12 -0800

    Removed / from Snapshots dir name

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@
 import java.io.File;
 
 public class Main {
-    public static final String SNAPSHOTS_DIR = "Snapshots/";
+    public static final String SNAPSHOTS_DIR = "Snapshots";
 
     public static void main(String... args) {
         File snapshotsDirectory = new File(SNAPSHOTS_DIR);
